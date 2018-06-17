5b845a61b6ece7d425468633e7a08c01a792a7cc
Danny Peng <dannyw.peng@gmail.com>
2018-06-17 16:32:44 -0700

    calculated x positions of 7 columns

--- connectFour.js
+++ connectFour.js
@@ -1,11 +1,25 @@
 const canvas = document.getElementById("connect4Canvas");
 const ctx = canvas.getContext("2d");
 
-let discWidth = (canvas.width/7)/2;
-let column1 = canvas.width/7 - discWidth;
-let column2 = (2*column1) - discWidth;
-let column3 = (3*column1) - discWidth;
-let column4 = (4*column1) - discWidth;
-let column5 = (5*column1) - discWidth;
-let column6 = (6*column1) - discWidth;
-let column7 = canvas.width - discWidth;
+let discDiameter = 80;
+let column1 = 10;
+let column2 = 20 + discDiameter;
+let column3 = 30 + 2*discDiameter;
+let column4 = 40 + 3*discDiameter;
+let column5 = 50 + 4*discDiameter;
+let column6 = 60 + 5*discDiameter;
+let column7 = 70 + 6*discDiameter;
+
+const drawDiscs = window.onload = function() {
+  let red = document.getElementById("red-disc");
+  ctx.drawImage(red, column1, (canvas.height - discDiameter), discDiameter, discDiameter);
+  let yellow = document.getElementById("yellow-disc");
+  ctx.drawImage(yellow, column2, (canvas.height - discDiameter), discDiameter, discDiameter);
+};
+
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawDiscs();
+};
+
+setInterval(draw, 8);
